a58af9023ad56acfcb351f98800499366d53f067
Ishaan Singal <ishaansingal@gmail.com>
2013-08-25 16:10:45 +0800

    highlighting facilities

--- ColorPallete/ColorPallete/ViewController.m
+++ ColorPallete/ColorPallete/ViewController.m
@@ -30,9 +30,9 @@
     [self.infController setDelegate:self];
     popover = [[UIPopoverController alloc]initWithContentViewController:self.infController];
 
-    UILongPressGestureRecognizer *longPressRecongizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(drawStuff:)];
-    [longPressRecongizer setMinimumPressDuration:0.0];
-    [longPressRecongizer setDelegate:self];
+//    UILongPressGestureRecognizer *longPressRecongizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(drawStuff:)];
+//    [longPressRecongizer setMinimumPressDuration:0.0];
+//    [longPressRecongizer setDelegate:self];
 
     [self loadSavePopup];
 //    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(drawStuff:)];
@@ -71,9 +71,10 @@
     CGContextRef currentContext = UIGraphicsGetCurrentContext();
     [self.displayImageView.image drawInRect:CGRectMake(0, 0, screenSize.width, screenSize.height)];
     
-    CGContextSetLineCap(currentContext, kCGLineCapRound);
-    CGContextSetLineWidth(currentContext, 10.0);
+//    CGContextSetLineCap(currentContext, kCGLineCapRound);
+    CGContextSetLineWidth(currentContext, 1.0);
     CGContextSetStrokeColorWithColor(currentContext, (self.colorSelectedButton.backgroundColor).CGColor);
+//    CGContextSetAlpha(currentContext, 0.2f);
     CGContextBeginPath(currentContext);
     CGContextMoveToPoint(currentContext, lastPoint.x, lastPoint.y);
     CGContextAddLineToPoint(currentContext, currentPoint.x, currentPoint.y);
@@ -81,10 +82,9 @@
     CGContextStrokePath(currentContext);
     
     self.displayImageView.image = UIGraphicsGetImageFromCurrentImageContext();
-    //        [self.displayImageView setAlpha:1];
+    [self.displayImageView setAlpha:1];
     
     UIGraphicsEndImageContext();
-    
     lastPoint = currentPoint;
 }
 
@@ -101,7 +101,8 @@
         
         CGContextSetLineCap(currentContext, kCGLineCapRound);
         CGContextSetLineWidth(currentContext, 10.0);
-        CGContextSetStrokeColorWithColor(currentContext, (self.colorSelectedButton.backgroundColor).CGColor);
+
+        CGContextSetStrokeColorWithColor(currentContext, [UIColor colorWithRed:255 green:255 blue:0 alpha:0.4f].CGColor);
         CGContextBeginPath(currentContext);
         CGContextMoveToPoint(currentContext, lastPoint.x, lastPoint.y);
         CGContextAddLineToPoint(currentContext, currentPoint.x, currentPoint.y);
@@ -137,7 +138,8 @@
 }
 
 - (IBAction)savePressed:(id)sender {
-    [self.savePopup show];
+    [self setPDFPage];
+//    [self.savePopup show];
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
@@ -145,6 +147,8 @@
     NSString *buttonTitle=[alertView buttonTitleAtIndex:buttonIndex];
     if (alertView == self.savePopup) {
         if([buttonTitle isEqualToString:@"Save"]) {
+            [self testMethod];
+            
             NSString* inputFileName = [[alertView textFieldAtIndex:0] text];
             inputFileName = [inputFileName stringByAppendingString:@".jpg"];
             
@@ -165,6 +169,109 @@
     }
 }
 
+- (void)setPDFPage
+{
+    NSURL *pdfURL = [[NSBundle mainBundle] URLForResource:@"Component List" withExtension:@"pdf"];
+    CGPDFDocumentRef PDFDocument = CGPDFDocumentCreateWithURL((__bridge CFURLRef)pdfURL);
+    CGPDFPageRef PDFPage = CGPDFDocumentGetPage(PDFDocument, 1);
+
+    // Determine the size of the PDF page.
+    CGRect pageRect = CGPDFPageGetBoxRect(PDFPage, kCGPDFMediaBox);
+    CGFloat PDFScale = self.view.frame.size.width/pageRect.size.width;
+    pageRect.size = CGSizeMake(pageRect.size.width * PDFScale, pageRect.size.height * PDFScale);
+    
+    
+    /*
+     Create a low resolution image representation of the PDF page to display before the TiledPDFView renders its content.
+     */
+    UIGraphicsBeginImageContext(pageRect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    // First fill the background with white.
+    CGContextSetRGBFillColor(context, 1.0,1.0,1.0,1.0);
+    CGContextFillRect(context,pageRect);
+    
+    CGContextSaveGState(context);
+    // Flip the context so that the PDF page is rendered right side up.
+    CGContextTranslateCTM(context, 0.0, pageRect.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    // Scale the context so that the PDF page is rendered at the correct size for the zoom level.
+    CGContextScaleCTM(context, PDFScale, PDFScale);
+    CGContextDrawPDFPage(context, PDFPage);
+    CGContextRestoreGState(context);
+    
+//    UIImage *backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+    self.displayImageView.image = UIGraphicsGetImageFromCurrentImageContext();
+
+    UIGraphicsEndImageContext();
+    
+//    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:backgroundImage];
+//    backgroundImageView.frame = pageRect;
+//    backgroundImageView.contentMode = UIViewContentModeScaleAspectFit;
+//    self.displayImageView = backgroundImageView;    
+}
+
+-(void)testMethod {
+    NSError *error;
+    //Load original pdf data:
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,  YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *originalPdfPath = [documentsDirectory stringByAppendingPathComponent:@"Component List.pdf"];
+    
+    NSData *pdfData = [NSData dataWithContentsOfFile:originalPdfPath options:NSDataReadingUncached error:&error];
+    
+    if (error)
+    {
+        NSLog(@"%@", [error localizedDescription]);
+        return;
+    }
+    else
+        NSLog(@"Data has loaded successfully.");
+    
+//    //If fails to create the new file, return    
+    NSString *NewPdfPath = [documentsDirectory stringByAppendingPathComponent:@"Edited List.pdf"];
+
+    if (![[NSFileManager defaultManager] createFileAtPath:NewPdfPath contents:pdfData attributes:nil])
+    {
+        return;
+    }
+    
+    NSURL *url = [NSURL fileURLWithPath:originalPdfPath];
+    CGPDFDocumentRef document = CGPDFDocumentCreateWithURL ((__bridge_retained CFURLRef) url);
+    size_t count = CGPDFDocumentGetNumberOfPages(document);
+    
+    if (count == 0)
+    {
+        NSLog(@"PDF needs at least one page");
+        return;
+    }
+    
+//    CGRect pageRect = CGPDFPageGetBoxRect(_PDFPage, kCGPDFMediaBox);
+
+    CGRect paperSize = CGRectMake(0, 0, 768, 1024);
+    
+    UIGraphicsBeginPDFContextToFile(NewPdfPath , paperSize, nil);
+    // CGPDFPageRef page = CGPDFDocumentGetPage(document, 1);
+    UIGraphicsBeginPDFPageWithInfo(paperSize, nil);
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    
+    CGContextTranslateCTM(currentContext, 0, paperSize.size.height);
+    CGContextScaleCTM(currentContext, 1.0, -1.0);
+    
+    CGPDFPageRef page = CGPDFDocumentGetPage (document, 1); // grab page 1 of the PDF
+    
+    CGContextDrawPDFPage (currentContext, page); // draw page 1 into graphics context
+    
+    // flip context so annotations are right way up
+    CGContextScaleCTM(currentContext, 1.0, -1.0);
+    CGContextTranslateCTM(currentContext, 0, -paperSize.size.height);
+    
+    //Render the layer of the annotations view in the context
+    [self.displayImageView.layer renderInContext:currentContext];
+    UIGraphicsEndPDFContext();
+}
 
 - (void)colorPickerControllerDidChangeColor:(InfColorPickerController *)controller {
     self.colorSelectedButton.backgroundColor = controller.resultColor;
